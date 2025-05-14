281f65226ab5e73c6255d09e0d15a22b25d00fb8
Laszlo Lazuer <visualjazz@gmail.com>
2025-05-13 21:45:18 -0600

    chore: add and configure eslint-plugin-jsx-a11y

--- eslint.config.js
+++ eslint.config.js
@@ -3,15 +3,32 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      { // Wrap the jsx-a11y extension in an object
+        plugins: {
+          'jsx-a11y': jsxA11y,
+        },
+        rules: {
+          ...jsxA11y.configs.recommended.rules,
+        },
+      },
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -23,6 +40,8 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      // You can still add or override specific jsx-a11y rules here if needed
+      // 'jsx-a11y/rule-name': 'error',
     },
   },
 )
